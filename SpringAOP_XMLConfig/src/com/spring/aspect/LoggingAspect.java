10b3953ee54f13729fa788e424a830af4dd7ff33
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-07 08:04:53 +0530

    Add files via upload

---
+++ SpringAOP_XMLConfig/src/com/spring/aspect/LoggingAspect.java
@@ -0,0 +1,23 @@
+package com.spring.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class LoggingAspect {
+
+	
+	public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+		Object returnvalue = null;
+		try {
+			System.out.println("Before returning");
+			returnvalue = proceedingJoinPoint.proceed();
+			System.out.println("After returning");
+		} catch (Throwable e) {
+			System.out.println("After throwing");
+		}
+		System.out.println("after finally");
+		return returnvalue;
+	}
+
+	
+
+}
